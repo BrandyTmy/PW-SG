f5374a85e07e68b3688dbb82bfaf0edcd9eac449
Brandy <brandyyyy5404@gmail.com>
2023-08-12 14:58:17 -0400

    <Completed JS>

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,33 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript']; 
+var randomTopic = topics[Math.floor(Math.random() * topics.length)]; 
+function ListTopics() { 
 
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 } 
+}
 
-/*
-if (topic == 'HTML') {
+function selectTopic() {
+
+
+if (randomTopic == 'HTML') {
     console.log("Lets study HTML!");
-}else if (topic === 'CSS') {
+}else if (randomTopic === 'CSS') {
     console.log("Lets study CSS!")
-}else if(topic === 'Git') {
+}else if(randomTopic === 'Git') {
     console.log("Lets study Git!");
-}else if (topic === 'Javascript'){
+}else if (randomTopic === 'Javascript'){
     console.log("Lets study Javascript!");
 }else{
     console.log('Please try again!');
+
+    
 }
-*/ 
+}
+console.log('Here are the topics we learned through Prework:');
+
+ListTopics();
+
+console.log('Which topic should we study first?');
+ 
+selectTopic();
